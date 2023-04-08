ee1d4a1fe6d1634cfac929ed6a4fdf461ee745f5
jingbo1.chen <jingbo1.chen@tcl.com>
2023-04-08 19:11:47 +0800

    Site updated: 2023-04-08 19:11:46

---
+++ js/title.js
@@ -0,0 +1,17 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = '👀不要走嘛~';
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = '🐖欢迎你回来～';
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
